7cf9ef8a41fa2b5e80432ecc0467b27fdbc40a56
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2022-09-27 10:03:45 +0100

    add kyu

--- 7kyu/sumOfNumbers.js
+++ 7kyu/sumOfNumbers.js
@@ -3,3 +3,5 @@ function getSum(a, b) {
     arr = Array(b - a + 1).fill(a).map((e, i) => e + i).reduce((acc, v) => acc + v, 0)
     return arr
 }
+
+// Optimization: Could have use the n(n+1)/2 formula
